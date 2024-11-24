c5947549cf50e7d9e36ef4c3e790a71b4ca8b620
Niccar95 <niccar.95@outlook.com>
2024-11-24 20:27:22 +0100

    🚧 succesfully created a test user and started setup for auth and login page

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,21 @@
+import prisma from "@/app/db";
+
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+export const POST = async (req: Request) => {
+  try {
+    const { name, email, image, password = "" } = await req.json();
+    if (!name || !password || !email)
+      return NextResponse.json({ message: "Invalid data" }, { status: 422 });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: { name, email, image: image || "", hashedPassword },
+    });
+    return NextResponse.json({ user }, { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Server error" }, { status: 500 });
+  }
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,68 @@
-export default function Home() {
+"use client";
+import React, { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+
+const Login = () => {
+  const [userName, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [loader, setLoader] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    setLoader(true);
+
+    const result = await signIn("credentials", {
+      redirect: false,
+      userName,
+      password,
+    });
+
+    setLoader(false);
+
+    if (result?.error) {
+      setErrorMessage("Invalid username or password.");
+    } else {
+      router.push("/home");
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="userName">Username:</label>
+        <input
+          id="userName"
+          className="textInput"
+          type="text"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        ></input>
+
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          className="textInput"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+
+        {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+
+        <button type="submit">Login</button>
+
+        {loader && (
+          <div className="spinnerWrapper">
+            <div className="spinner"></div>
+          </div>
+        )}
+      </form>
+    </>
   );
-}
+};
+
+export default Login;
