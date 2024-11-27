3b73e9729ffb7f8ea5536e35d874ece1d01818df
Niccar95 <niccar.95@outlook.com>
2024-11-27 08:47:36 +0100

    Added neaxtauth file and interface augmentation for user

---
+++ next-auth.d.ts
@@ -0,0 +1,15 @@
+import type { DefaultSession } from "next-auth";
+declare module "next-auth" {
+  interface User {
+    id: string;
+  }
+
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    } & DefaultSession["user"];
+  }
+}
