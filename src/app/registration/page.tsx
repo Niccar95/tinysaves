c960f2ba136502188a0065e57959eadda9507698
Niccar95 <niccar.95@outlook.com>
2024-11-27 21:14:26 +0100

    added joi client side validation for registration

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,22 +1,64 @@
 "use client";
-import React, { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import Joi from "joi";
+
+const schema = Joi.object({
+  name: Joi.string().min(3).required().label("Name"),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .label("Email"),
+  password: Joi.string().min(6).required().label("Password"),
+  repeatPassword: Joi.valid(Joi.ref("password"))
+    .required()
+    .label("Repeat Password")
+    .messages({
+      "any.only": "Passwords must match",
+    }),
+});
 
 const Page = () => {
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [repeatPassword, setRepeatPassword] = useState<string>("");
+  const router = useRouter();
+
+  const [formData, setFormData] = useState<{
+    name: string;
+    email: string;
+    password: string;
+    repeatPassword: string;
+  }>({
+    name: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setError(null);
+  };
 
   const registerUser = async (e: FormEvent) => {
     e.preventDefault();
+
+    const { error } = schema.validate(formData, { abortEarly: false });
+    if (error) {
+      setError(error.details.map((err) => err.message).join(", "));
+      return;
+    }
+
     try {
       await fetch("/api/register", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
         },
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ formData }),
       });
+
+      router.push("/");
     } catch (error) {
       console.log("Could not create account", error);
     }
@@ -29,23 +71,26 @@ const Page = () => {
         <input
           id="userName"
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
         />
         <label htmlFor="userEmail">Type your email</label>
         <input
           id="userEmail"
           type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
         />
 
         <label htmlFor="userPassword">Create a strong password</label>
         <input
           id="userPassword"
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
         />
 
         <label htmlFor="repeatedUserPassword">Repeat your password</label>
@@ -53,19 +98,15 @@ const Page = () => {
         <input
           id="repeatedUserPassword"
           type="password"
-          value={repeatPassword}
-          onChange={(e) => setRepeatPassword(e.target.value)}
+          name="repeatPassword"
+          value={formData.repeatPassword}
+          onChange={handleChange}
         />
-        {repeatPassword && (
-          <div
-            style={{
-              color: password === repeatPassword ? "green" : "red",
-            }}
-          >
-            {password === repeatPassword
-              ? "Passwords match!"
-              : "Passwords do not match!"}
-          </div>
+
+        {error && <div style={{ color: "red" }}>{error}</div>}
+
+        {formData.password && formData.password === formData.repeatPassword && (
+          <div style={{ color: "green" }}>Passwords match!</div>
         )}
         <button type="submit">Register</button>
       </form>
