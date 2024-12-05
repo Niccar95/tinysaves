d6f7fdccdde2c8786e5382db0e14a2854211c142
Niccar95 <niccar.95@outlook.com>
2024-12-05 21:15:12 +0100

    added line chart and correct data to it

--- src/app/components/Charts.tsx
+++ src/app/components/Charts.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Chart, registerables } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Line } from "react-chartjs-2";
 
 Chart.register(...registerables);
 
@@ -11,6 +11,7 @@ interface ChartProps {
   totalGoals: number;
   totalSaved: number;
   completedPercentage: number;
+  creationDates: string[];
 }
 
 const Charts = ({
@@ -18,23 +19,55 @@ const Charts = ({
   totalGoals,
   totalSaved,
   completedPercentage,
+  creationDates,
 }: ChartProps) => {
-  const test = {
-    labels: ["Not completed", "Completed"],
+  const doughNutData = {
+    labels: ["Completed", "Not completed"],
     datasets: [
       {
-        label: "Goals Completion",
+        label: "Goals",
         data: [completedGoals, totalGoals - completedGoals],
         backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
         hoverOffset: 4,
       },
     ],
   };
+
+  const dateCounts = creationDates.reduce(
+    (acc: { [key: string]: number }, date: string) => {
+      acc[date] = (acc[date] || 0) + 1;
+      return acc;
+    },
+    {} as { [key: string]: number }
+  );
+
+  const lineLabels = Object.keys(dateCounts);
+  const lineDataValues = Object.values(dateCounts);
+
+  const lineData = {
+    labels: lineLabels,
+    datasets: [
+      {
+        label: "Added goals",
+        data: lineDataValues,
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  };
+
   return (
     <>
-      <div className="doughnutChartContainer">
-        <Doughnut data={test} />
-      </div>
+      <section className="chartSection">
+        <div className="doughnutChartContainer">
+          <Doughnut data={doughNutData} />
+        </div>
+
+        <div className="lineChartContainer">
+          <Line data={lineData} />
+        </div>
+      </section>
     </>
   );
 };
--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -30,21 +30,29 @@ const Page = async () => {
   const completedPercentage = (completedGoals.length / allGoals.length) * 100;
   const totalSaved = allGoals.reduce((total, goal) => total + goal.progress, 0);
 
+  const creationDates = allGoals.map((goal) =>
+    new Date(goal.createdAt).toLocaleDateString()
+  );
+
   console.log(allGoals);
+  console.log(creationDates);
 
   return (
     <>
       <section className="content">
-        <h1>My stats</h1>
-        <h2>Amount of added goals: {allGoals.length}</h2>
-        <h2>Completed goals: {completedGoals.length}</h2>
-        <h2>Percentage of completed goals: {completedPercentage}</h2>
-        <h2>Amount of money saved: {totalSaved}</h2>
+        <section className="statSummarySection">
+          <h1>My stats</h1>
+          <p>Amount of added goals: {allGoals.length}</p>
+          <p>Completed goals: {completedGoals.length}</p>
+          <p>Percentage of completed goals: {completedPercentage}</p>
+          <p>Amount of money saved: {totalSaved}</p>
+        </section>
         <Charts
           completedGoals={completedGoals.length}
           totalGoals={allGoals.length}
           totalSaved={totalSaved}
           completedPercentage={completedPercentage}
+          creationDates={creationDates}
         />
       </section>
     </>
