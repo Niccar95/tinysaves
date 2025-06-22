84c20700b9d10eb2a9f5e6524559037da566b4f3
Niccar95 <niccar.95@outlook.com>
2025-06-22 23:33:07 +0200

    small wteaks to lang menu

--- src/app/components/ConditionalHeader.tsx
+++ src/app/components/ConditionalHeader.tsx
@@ -6,12 +6,17 @@ import logo from "/public/logo.svg";
 import LanguageMenu from "./LanguageMenu";
 import { useState } from "react";
 import { useLocale } from "next-intl";
+import { useClickOutside } from "../hooks/useClickOutside";
 
 const ConditionalHeader = () => {
   const locale = useLocale();
   const [openLangMenu, setOpenLangMenu] = useState<boolean>(false);
   const [currentLang, setCurrentLang] = useState<string>(locale);
   const pathname = usePathname();
+
+  const actionsMenuRef = useClickOutside<HTMLDivElement>({
+    onClickOutside: () => setOpenLangMenu(false),
+  });
 
   if (
     pathname === "/" ||
@@ -24,33 +29,36 @@ const ConditionalHeader = () => {
   return (
     <header>
       <Image className="logo" src={logo} alt="icon"></Image>
-      <button
-        className="langMenuButton"
-        onClick={() => {
-          setOpenLangMenu(!openLangMenu);
-        }}
-      >
-        {currentLang == "en" && (
-          <Image src={"/en.svg"} width={20} height={20} alt="en"></Image>
-        )}
-        {currentLang == "sv" && (
-          <Image src={"/sv.svg"} width={20} height={20} alt="sv"></Image>
-        )}
-        {currentLang == "es" && (
-          <Image src={"/es.svg"} width={20} height={20} alt="es"></Image>
-        )}
-        {currentLang.toUpperCase()}
-        <i
-          className={`bi bi-caret-down-fill ${openLangMenu ? "rotate" : ""}`}
-        ></i>
-      </button>
 
-      {openLangMenu && (
-        <LanguageMenu
-          setCurrentLang={setCurrentLang}
-          closeMenu={() => setOpenLangMenu(false)}
-        />
-      )}
+      <div ref={actionsMenuRef} onClick={(e) => e.stopPropagation()}>
+        <button
+          className="langMenuButton"
+          onClick={() => {
+            setOpenLangMenu(!openLangMenu);
+          }}
+        >
+          {currentLang == "en" && (
+            <Image src={"/en.svg"} width={20} height={20} alt="en"></Image>
+          )}
+          {currentLang == "sv" && (
+            <Image src={"/sv.svg"} width={20} height={20} alt="sv"></Image>
+          )}
+          {currentLang == "es" && (
+            <Image src={"/es.svg"} width={20} height={20} alt="es"></Image>
+          )}
+          {currentLang.toUpperCase()}
+          <i
+            className={`bi bi-caret-down-fill ${openLangMenu ? "rotate" : ""}`}
+          ></i>
+        </button>
+
+        {openLangMenu && (
+          <LanguageMenu
+            setCurrentLang={setCurrentLang}
+            closeMenu={() => setOpenLangMenu(false)}
+          />
+        )}
+      </div>
     </header>
   );
 };
