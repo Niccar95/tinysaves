5c1dba751425bd806051f0554d4e0ad754f71d4e
Niccar95 <niccar.95@outlook.com>
2025-06-23 00:04:50 +0200

    navbar can now be closed by clicking ouside of it on mobile

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,12 +2,25 @@
 
 import { signOut } from "next-auth/react";
 import Link from "next/link";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { SidebarContext } from "../contexts/SidebarContext";
+import { useClickOutside } from "../hooks/useClickOutside";
 
 const Navbar = () => {
   const [isNavClicked, setIsNavClicked] = useState<boolean>(false);
   const { isSidebarOpen, setIsSidebarOpen } = useContext(SidebarContext);
+
+  const windowCheckRef = useRef<boolean>(false);
+  const [windowCheck, setwindowCheck] = useState<boolean>(false);
+
+  const actionsMenuRef = useClickOutside<HTMLDivElement>({
+    onClickOutside: () => {
+      if (!windowCheck) {
+        setIsSidebarOpen(false);
+        setIsNavClicked(false);
+      }
+    },
+  });
 
   const burgerClass = isNavClicked
     ? "burgerBar clicked"
@@ -35,6 +48,8 @@ const Navbar = () => {
 
   useEffect(() => {
     const isDesktop = window.innerWidth >= 1200;
+    setwindowCheck(isDesktop);
+    windowCheckRef.current = isDesktop;
     setIsSidebarOpen(isDesktop);
   }, [setIsSidebarOpen]);
 
@@ -47,64 +62,70 @@ const Navbar = () => {
           <div className={burgerClass}></div>
         </div>
       </section>
-      <nav className={`navBar ${isSidebarOpen ? "visible" : "hidden"}`}>
-        <div className="sidebarToggleContainer">
-          <button
-            className="sidebarToggle"
-            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          >
-            <i
-              className={`bi bi-caret-right-fill ${
-                isSidebarOpen ? "rotate" : ""
-              }`}
-            ></i>
-          </button>
-        </div>
-        <ul>
-          <li>
-            <Link className="navLink" href="/dashboard" onClick={closeNavBar}>
-              <i className="bi bi-house-door-fill"></i>
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link className="navLink" href="/goals" onClick={closeNavBar}>
-              <i className="bi bi-piggy-bank-fill"></i>
-              My goals
-            </Link>
-          </li>
-          <li>
-            <Link className="navLink" href="/milestones" onClick={closeNavBar}>
-              <i className="bi bi-trophy-fill"></i>
-              My milestones
-            </Link>
-          </li>
-          <li>
-            <Link className="navLink" href="/stats" onClick={closeNavBar}>
-              <i className="bi bi-bar-chart-line-fill"></i>
-              My stats
-            </Link>
-          </li>
-          <li>
-            <Link className="navLink" href="/profile" onClick={closeNavBar}>
-              <i className="bi bi-person-circle"></i>
-              My profile
-            </Link>
-          </li>
-          <li>
-            <Link className="navLink" href="/settings" onClick={closeNavBar}>
-              <i className="bi bi-gear-fill"></i>
-              Settings
-            </Link>
-          </li>
-        </ul>
-        <section className="logoutButtonSection">
-          <button className="logoutButton" onClick={handleLogout}>
-            <i className="bi bi-box-arrow-left"></i>
-            Log out
-          </button>
-        </section>
-      </nav>
+      <div ref={actionsMenuRef} onClick={(e) => e.stopPropagation()}>
+        <nav className={`navBar ${isSidebarOpen ? "visible" : "hidden"}`}>
+          <div className="sidebarToggleContainer">
+            <button
+              className="sidebarToggle"
+              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+            >
+              <i
+                className={`bi bi-caret-right-fill ${
+                  isSidebarOpen ? "rotate" : ""
+                }`}
+              ></i>
+            </button>
+          </div>
+          <ul>
+            <li>
+              <Link className="navLink" href="/dashboard" onClick={closeNavBar}>
+                <i className="bi bi-house-door-fill"></i>
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" href="/goals" onClick={closeNavBar}>
+                <i className="bi bi-piggy-bank-fill"></i>
+                My goals
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="navLink"
+                href="/milestones"
+                onClick={closeNavBar}
+              >
+                <i className="bi bi-trophy-fill"></i>
+                My milestones
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" href="/stats" onClick={closeNavBar}>
+                <i className="bi bi-bar-chart-line-fill"></i>
+                My stats
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" href="/profile" onClick={closeNavBar}>
+                <i className="bi bi-person-circle"></i>
+                My profile
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" href="/settings" onClick={closeNavBar}>
+                <i className="bi bi-gear-fill"></i>
+                Settings
+              </Link>
+            </li>
+          </ul>
+          <section className="logoutButtonSection">
+            <button className="logoutButton" onClick={handleLogout}>
+              <i className="bi bi-box-arrow-left"></i>
+              Log out
+            </button>
+          </section>
+        </nav>
+      </div>
     </>
   );
 };
