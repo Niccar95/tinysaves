7a48d45b18ec0255003969a43073ce614b6d41d0
Niccar95 <niccar.95@outlook.com>
2025-08-01 15:20:20 +0200

    changed banner color for dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,15 @@ import Image from "next/image";
 import LoginForm from "./components/LoginForm";
 import InstallButton from "./components/InstallButton";
 import { useTranslations } from "next-intl";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
+import bannerLightMode from "/public/banner-lightMode.svg";
+import bannerDarkMode from "/public/banner-darkMode.svg";
 
 const Login = () => {
   const router = useRouter();
+
+  const { trackedTheme } = useContext(ThemeContext);
 
   const t = useTranslations("loginPage");
 
@@ -42,7 +47,22 @@ const Login = () => {
           <InstallButton />
         </section>
         <div className="bannerWrapper">
-          <Image className="banner" src="/banner.svg" alt="banner" fill></Image>
+          {trackedTheme === "light" && (
+            <Image
+              className="banner"
+              src={bannerLightMode}
+              alt="banner"
+              fill
+            ></Image>
+          )}
+          {trackedTheme === "dark" && (
+            <Image
+              className="banner"
+              src={bannerDarkMode}
+              alt="banner"
+              fill
+            ></Image>
+          )}
         </div>
       </div>
     </>
