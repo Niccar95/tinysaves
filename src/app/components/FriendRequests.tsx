245aafebeb47b4add630c406cd5fbe0f851315d3
Niccar95 <niccar.95@outlook.com>
2025-08-29 16:03:45 +0200

    notificatins should work as expected

--- src/app/components/FriendRequests.tsx
+++ src/app/components/FriendRequests.tsx
@@ -7,18 +7,27 @@ import { NotificationsContext } from "../contexts/NotificationsContext";
 const FriendRequests = () => {
   const { notifications } = useContext(NotificationsContext);
   const latestNotificationRef = useRef<string | null>(null);
+  const hasBeenShown = useRef(false);
 
   useEffect(() => {
     const friendRequests = notifications.filter(
       (notification) => notification.type === "friend_request"
     );
+
     if (friendRequests.length === 0) return;
 
     const latestNotification = friendRequests[0];
 
+    if (!hasBeenShown.current) {
+      latestNotificationRef.current = latestNotification.notificationId;
+      hasBeenShown.current = true;
+      return;
+    }
+
     if (latestNotification.notificationId !== latestNotificationRef.current) {
       latestNotificationRef.current = latestNotification.notificationId;
-      toast.info(`New friend request from ${latestNotification.fromUserId}`);
+
+      toast.info(latestNotification.message);
     }
   }, [notifications]);
 
